4b1d6a42be7bdafcd5f431e4a6dc7496091c757d
SaifJava575 <sksaifuddin575@gmail.com>
2022-05-12 14:42:36 +0530

    setup project

---
+++ src/main/java/com/nit/saif/HealtCareApplicationInAngularApplication.java
@@ -0,0 +1,13 @@
+package com.nit.saif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtCareApplicationInAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealtCareApplicationInAngularApplication.class, args);
+	}
+
+}
